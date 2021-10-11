10be02a04112c7da168d1bbb481a0218efc4da78
Sayat <sayatkazaryan@mail.ru>
2021-10-11 16:39:05 +0400

    init

---
+++ src/com/company/homework/FigurePainter3.java
@@ -0,0 +1,15 @@
+package com.company.homework;
+
+public class FigurePainter3 {
+    public static void main(String[] args) {
+        for (int i = 3; i >=0; i--) {
+            for (int j = 0; j <= (3 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
